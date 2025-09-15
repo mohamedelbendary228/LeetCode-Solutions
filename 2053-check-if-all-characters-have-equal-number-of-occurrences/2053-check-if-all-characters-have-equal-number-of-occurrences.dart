c6682f0a37618b53c413721e4b0451bc96afe3fa
Mohamed Elbendary <37798971+mohamedelbendary228@users.noreply.github.com>
2025-09-15 16:49:07 +0300

    Time: 5 ms (90%), Space: 150.1 MB (20%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.dart
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.dart
@@ -4,12 +4,7 @@ bool areOccurrencesEqual(String s) {
   Map<String, int> frMap = {};
   
   for (int i = 0; i < s.length; i++) {
-    final char = s[i];
-    if (frMap.containsKey(char)) {
-      frMap[char] = (frMap[char] ?? 0) + 1;
-    } else {
-      frMap[char] = 1;
-    }
+    frMap[s[i]] = (frMap[s[i]] ?? 0) + 1;
   }
 
   final firstCount = frMap.values.first;
