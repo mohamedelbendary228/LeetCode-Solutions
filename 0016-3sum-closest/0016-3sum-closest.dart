7fc9924ede9ff6c82fbede52c680db15133f9c43
Mohamed Elbendary <37798971+mohamedelbendary228@users.noreply.github.com>
2025-11-11 15:27:11 +0200

    Time: 19 ms (100%), Space: 154.4 MB (45.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.dart
+++ 0016-3sum-closest/0016-3sum-closest.dart
@@ -2,7 +2,7 @@ class Solution {
   int threeSumClosest(List<int> nums, int target) {
     nums.sort();
     int closestSum = nums[0] + nums[1] + nums[2];
-    for (int i = 0; i < nums.length - 2; i++) {
+    for (int i = 0; i < nums.length; i++) {
         int left = i + 1;
         int right = nums.length - 1;
         while (left < right) {
